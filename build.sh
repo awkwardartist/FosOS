7c7da092d6e2037c68346d3db7f66a7bb5738ac8
awkwardartist <73862490+awkwardartist@users.noreply.github.com>
2021-11-10 17:14:16 -0500

    update build script

--- build.sh
+++ build.sh
@@ -1,8 +1,9 @@
+sudo su
 source ./.bashrc
 
 nasm -felf OS/kernel.asm -o kboot.o
 gcc -m32 -fno-stack-protector -Wl,static-libgcc,lgcc -ffreestanding -fpermissive -c OS/ckernel.cpp -w -o kernel.o -I ./OS/lib
-ld -m elf_i386 -T OS/link.ld -o kernel_execute kboot.o kernel.o
+ld -m elf_i386 -T ./OS/link.ld -o ./OS/kernel_execute kboot.o kernel.o
 qemu-system-i386 -kernel ./OS/kernel_execute -m 512
 
 rm -f *.bin *.o
