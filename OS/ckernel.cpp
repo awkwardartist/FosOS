8f12c08c3bac9393aa1f469383803be569f11823
awkwardartist <73862490+awkwardartist@users.noreply.github.com>
2021-11-10 17:10:18 -0500

    Init

---
+++ OS/ckernel.cpp
@@ -0,0 +1,5 @@
+extern "C" void Kernel();
+
+void Kernel() {
+
+}
---
+++ build.sh
@@ -0,0 +1,9 @@
+source ./.bashrc
+
+
+nasm -felf OS/kernel.asm -o kboot.o
+gcc -m32 -fno-stack-protector -Wl,static-libgcc,lgcc -ffreestanding -fpermissive -c OS/ckernel.cpp -w -o kernel.o -I ./OS/lib
+ld -m elf_i386 -T link.ld -o kernel_execute kboot.o kernel.o
+qemu-system-i386 -kernel ./OS/kernel_execute -m 512
+
+rm -f *.bin *.o
